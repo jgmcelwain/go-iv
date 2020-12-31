343a33f9b6da8c064e9adddcf1b85fe75ed7469f
Jamie McElwain <jgmcelwain@icloud.com>
2020-12-31 16:57:08 +0000

    feat: bottom margin so final league can't "touch" screen edge

--- components/Main.tsx
+++ components/Main.tsx
@@ -12,7 +12,7 @@ const Main: FunctionComponent = () => {
       <div className='container mx-auto px-4'>
         <SubjectBuilder />
 
-        <div className='w-full grid grid-cols-1 xl:grid-cols-2 gap-6'>
+        <div className='w-full grid grid-cols-1 xl:grid-cols-2 gap-6 mb-8'>
           {LEAGUES.map((league) => (
             <League key={league.key} league={league} />
           ))}
