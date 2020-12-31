2ba53bd6df333e3c33e9adc13cc1cc7e26c7678e
Jamie McElwain <jgmcelwain@icloud.com>
2020-12-31 17:56:40 +0000

    feat: add Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,24 @@
+import React, { FunctionComponent } from 'react';
+
+const Footer: FunctionComponent = () => (
+  <div className='w-full my-8 pt-8 px-4 border-t border-gray-800'>
+    <p className='text-center text-xs text-gray-400 mb-2'>
+      Pokémon are copyright of The Pokémon Company and Nintendo. All data used
+      in this application is for educational purposes only.
+    </p>
+
+    <p className='text-center text-xs text-gray-400'>
+      pvpiv.app is open source on GitHub -{' '}
+      <a
+        href='https://github.com/jgmcelwain/pvpiv.app'
+        rel='noreferrer'
+        target='_blank'
+        className='text-blue-400 hover:underline'
+      >
+        jgmcelwain/pvpiv.app
+      </a>
+    </p>
+  </div>
+);
+
+export default Footer;
--- components/Main.tsx
+++ components/Main.tsx
@@ -3,22 +3,27 @@ import React, { FunctionComponent } from 'react';
 import { LEAGUES } from '../data/reference';
 
 import { Wrapper as SubjectContextWrapper } from './SubjectContext';
+import Footer from './Footer';
 import League from './League';
 import SubjectBuilder from './SubjectBuilder';
 
 const Main: FunctionComponent = () => {
   return (
-    <SubjectContextWrapper>
-      <div className='container mx-auto'>
-        <SubjectBuilder />
+    <div className='container mx-auto'>
+      <SubjectContextWrapper>
+        <>
+          <SubjectBuilder />
 
-        <div className='w-full grid grid-cols-1 lg:grid-cols-2 gap-6 mb-8'>
-          {LEAGUES.map((league) => (
-            <League key={league.key} league={league} />
-          ))}
-        </div>
-      </div>
-    </SubjectContextWrapper>
+          <div className='w-full grid grid-cols-1 lg:grid-cols-2 gap-6 mb-8'>
+            {LEAGUES.map((league) => (
+              <League key={league.key} league={league} />
+            ))}
+          </div>
+        </>
+      </SubjectContextWrapper>
+
+      <Footer />
+    </div>
   );
 };
 
