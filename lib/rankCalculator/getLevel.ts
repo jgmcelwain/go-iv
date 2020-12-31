b2b2dbb9b690dcd877c6077c0482248231423249
Jamie McElwain <jgmcelwain@icloud.com>
2020-12-31 18:57:26 +0000

    refactor: use a while loop to get our cpm index
    similar performance to the binary search and much more radable

--- lib/rankCalculator/getLevel.ts
+++ lib/rankCalculator/getLevel.ts
@@ -12,32 +12,24 @@ export function getLevel(
   maxCP: LeagueCPCap,
   maxLevel: PowerUpLevelCap,
 ) {
-  function binarySearch(ar: number[], num: number) {
-    let m = 0;
-    let n = ar.length - 1;
+  function getCPMIndex(query: number) {
+    let i = 0;
 
-    while (m <= n) {
-      const k = (n + m) >> 1;
-
-      if (ar[k] < num) {
-        m = k + 1;
-      } else if (ar[k] > num) {
-        n = k - 1;
-      } else {
-        return k;
-      }
+    while (CPMs[i] <= query) {
+      i++;
     }
-    return m - 1;
+
+    return i - 1;
   }
 
-  const cpmIndex = binarySearch(
-    CPMs,
+  const cpmIndex = getCPMIndex(
     Math.pow(
-      ((maxCP + 1) * (maxCP + 1) * 100) / (Math.pow(atk, 2) * def * sta),
+      (100 * Math.pow(maxCP + 1, 2)) / (Math.pow(atk, 2) * def * sta),
       0.25,
     ),
   );
+
   const level = 1 + cpmIndex / 2;
 
-  return level > maxLevel ? maxLevel : level;
+  return Math.min(level, maxLevel);
 }
