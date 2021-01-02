41ee27e485283ee09d982098ad493be82fedd589
Jamie McElwain <jgmcelwain@icloud.com>
2021-01-02 20:05:44 +0000

    feat: add version to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,18 @@
 import React, { FunctionComponent } from 'react';
+import { version } from '../package.json';
 
 import HeaderSettings from './HeaderSettings';
 
 const Header: FunctionComponent = () => {
   return (
     <div className='w-full mb-2 bg-gray-800'>
-      <div className='container mx-auto px-4 flex justify-between items-center '>
-        <h1 className='font-title text-gray-200 text-xl py-4'>pvpiv.app</h1>
+      <div className='container mx-auto px-4 flex justify-between items-center py-4'>
+        <div className='font-title flex-grow flex justify-start items-center'>
+          <h1 className='text-gray-200 text-xl'>pvpiv.app</h1>
+          <span className='text-gray-600 text-sm ml-1 transform translate-y-0.5'>
+            v{version}
+          </span>
+        </div>
 
         <HeaderSettings />
       </div>
