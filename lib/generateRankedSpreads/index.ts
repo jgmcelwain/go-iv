f143e0700b1cb14126a888ee5bb6e053158cfe6f
Jamie McElwain <jgmcelwain@icloud.com>
2021-01-03 22:12:31 +0000

    fix: remove console.log

--- lib/generateRankedSpreads/index.ts
+++ lib/generateRankedSpreads/index.ts
@@ -14,7 +14,6 @@ export function generateRankedSpreads(
   maxCP: LeagueCPCap,
   maxLevel: LevelCapNumber,
 ) {
-  console.log(pokemon.id, floor, maxCP, maxLevel);
   return getIVSpreads(floor)
     .map((ivs) => getMaximizedStats(pokemon, ivs, maxCP, maxLevel))
     .sort((a, b) => {
