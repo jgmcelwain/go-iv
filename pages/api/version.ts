664006b26d6a3ed92910baab69bd05c6a3e81b73
Jamie McElwain <jgmcelwain@icloud.com>
2021-01-07 20:57:50 +0000

    feat: add version number API route

---
+++ pages/api/version.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { version } from '../../package.json';
+
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+
+  res.end(JSON.stringify({ version }));
+}
