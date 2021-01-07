2964561ca4940236a3e00d4071e411e8a346af2f
Jamie McElwain <jgmcelwain@icloud.com>
2021-01-07 20:26:22 +0000

    fix: stricter types in getIVSpreads

--- lib/generateRankedSpreads/getIVSpreads.ts
+++ lib/generateRankedSpreads/getIVSpreads.ts
@@ -1,11 +1,11 @@
-import { IVFloor, PokemonIVs } from '../../data/reference';
+import { IV, IVFloor, PokemonIVs } from '../../data/reference';
 
 export function getIVSpreads(floor: IVFloor) {
   const ivCombinations: PokemonIVs[] = [];
 
-  for (let atk = floor; atk <= 15; atk++) {
-    for (let def = floor; def <= 15; def++) {
-      for (let sta = floor; sta <= 15; sta++) {
+  for (let atk: IV = floor as IV; atk <= 15; atk++) {
+    for (let def: IV = floor as IV; def <= 15; def++) {
+      for (let sta: IV = floor as IV; sta <= 15; sta++) {
         ivCombinations.push({ atk, def, sta });
       }
     }
