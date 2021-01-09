973681298674090c69220971bdec1842895ed465
Jamie McElwain <jgmcelwain@icloud.com>
2021-01-09 18:56:36 +0000

    fix: higher contrast text for table cells

--- utils/getRankColors.ts
+++ utils/getRankColors.ts
@@ -11,14 +11,14 @@ export function getRankedSpreadColors(spread: RankedSpreadStats): SpreadColors {
   }
 
   if (spread.percent === 1) {
-    return { background: 'bg-purple-50', text: 'text-purple-700' };
+    return { background: 'bg-purple-50', text: 'text-purple-800' };
   } else if (spread.percent >= 0.98) {
-    return { background: 'bg-green-50', text: 'text-green-700' };
+    return { background: 'bg-green-50', text: 'text-green-800' };
   } else if (spread.percent >= 0.95) {
-    return { background: 'bg-yellow-50', text: 'text-yellow-700' };
+    return { background: 'bg-yellow-50', text: 'text-yellow-800' };
   } else if (spread.percent >= 0.9) {
-    return { background: 'bg-orange-50', text: 'text-orange-700' };
+    return { background: 'bg-orange-50', text: 'text-orange-800' };
   } else {
-    return { background: 'bg-red-50', text: 'text-red-700' };
+    return { background: 'bg-red-50', text: 'text-red-800' };
   }
 }
