02b7ada37b50e474995bc3fa86e71f6bfb8d3640
Jamie McElwain <jgmcelwain@icloud.com>
2021-04-26 22:09:15 +0100

    refactor: use @headlessui RadioGroup for family swapper

--- components/SubjectBuilderSpeciesFamilySwap.tsx
+++ components/SubjectBuilderSpeciesFamilySwap.tsx
@@ -5,6 +5,7 @@ import { Pokemon } from '../data/reference';
 import { useSubject } from '../hooks/useSubject';
 import { usePokedex } from '../hooks/usePokedex';
 
+import { RadioGroup } from '@headlessui/react';
 import { SwitchVerticalIcon } from '@heroicons/react/solid';
 
 const SubjectBuilderSpeciesFamilySwap: FunctionComponent<{
@@ -22,27 +23,43 @@ const SubjectBuilderSpeciesFamilySwap: FunctionComponent<{
   if (iterableFamily.length === 0) return null;
 
   return (
-    <div className='flex flex-wrap items-center justify-start order-2 w-full my-1 md:order-5'>
-      {iterableFamily.map((familyMember) => (
-        <button
-          key={familyMember.id}
-          onClick={() => {
-            onChange(familyMember);
-          }}
-          className={`flex justify-start items-center transition-colors border mr-2 mb-2 last:mr-0 py-1 px-2 focus-visible-ring ring-offset-gray-800 hover:text-blue-300 rounded bg-gray-800 ${
-            subject.species.id === familyMember.id
-              ? 'text-blue-300 border-gray-700'
-              : 'text-gray-400 border-transparent'
-          }`}
-        >
-          <span className='mr-2 text-sm font-semibold'>
-            {familyMember.name}
-          </span>
+    <RadioGroup
+      value={subject.species}
+      onChange={(val: Pokemon) => onChange(val)}
+      as='div'
+      className='flex flex-wrap items-center justify-start order-2 w-full my-1 md:order-5'
+    >
+      <RadioGroup.Label className='sr-only'>
+        Other family members
+      </RadioGroup.Label>
 
-          <SwitchVerticalIcon className='w-4 h-4' />
-        </button>
+      {iterableFamily.map((familyMember) => (
+        <RadioGroup.Option
+          key={familyMember.id}
+          value={familyMember}
+          className='mb-2 mr-2 rounded cursor-pointer last:mr-0 focus-visible-ring ring-offset-gray-800'
+        >
+          {({ checked }) => (
+            <div
+              className={`flex justify-start items-center transition-colors border py-1 px-2 hover:text-blue-300 rounded bg-gray-800 ${
+                checked
+                  ? 'text-blue-300 border-gray-700'
+                  : 'text-gray-400 border-transparent'
+              }`}
+            >
+              <RadioGroup.Label
+                className='mr-2 text-sm font-semibold'
+                as='span'
+              >
+                {familyMember.name}
+              </RadioGroup.Label>
+
+              <SwitchVerticalIcon className='w-4 h-4' />
+            </div>
+          )}
+        </RadioGroup.Option>
       ))}
-    </div>
+    </RadioGroup>
   );
 };
 
