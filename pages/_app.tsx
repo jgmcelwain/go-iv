a4e38741b7f9fefff089fa90a66f1620fc84e35f
Jamie McElwain <jgmcelwain@icloud.com>
2021-05-03 13:15:16 +0100

    feat: add social-sharing meta properties

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,27 +22,35 @@ const App: FunctionComponent<AppProps & InitialProps> & {
   getInitialProps: (arg0: AppContext) => Promise<InitialProps>;
 } = ({ Component, pageProps, settings }) => {
   return (
-    <SettingsProvider initialValue={settings}>
+    <>
       <Head>
         <title>PVPIV.app - Pokémon GO PVP IV Checker</title>
         <meta
           name='description'
           content='Check PVP IV ranks for Great League, Ultra League and Master League simultaneously. Includes support for Best Buddy and Candy XL level caps.'
         />
+
+        <meta property='og:title' content='PVPIV.app' />
+        <meta property='og:description' content='Pokémon GO PVP IV Checker' />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://pvpiv.app' />
+        <meta property='og:image' content='https://pvpiv.app/og.png' />
       </Head>
 
-      <div className='w-full pt-16 pl-0 md:pl-64 md:pt-0'>
-        <AppNavigation />
+      <SettingsProvider initialValue={settings}>
+        <div className='w-full pt-16 pl-0 md:pl-64 md:pt-0'>
+          <AppNavigation />
 
-        <section className='flex flex-col items-start justify-start flex-auto w-full min-h-screen px-0 pt-4 mx-auto max-w-8xl sm:px-4 md:px-8'>
-          <main className='flex-grow w-full'>
-            <Component {...pageProps} />
-          </main>
+          <section className='flex flex-col items-start justify-start flex-auto w-full min-h-screen px-0 pt-4 mx-auto max-w-8xl sm:px-4 md:px-8'>
+            <main className='flex-grow w-full'>
+              <Component {...pageProps} />
+            </main>
 
-          <AppFooter />
-        </section>
-      </div>
-    </SettingsProvider>
+            <AppFooter />
+          </section>
+        </div>
+      </SettingsProvider>
+    </>
   );
 };
 
