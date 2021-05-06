39b93bd9256e9f1ae0ee5493d28916751cca2f10
Jamie McElwain <jgmcelwain@icloud.com>
2021-05-06 23:08:20 +0100

    fix: rename "% of Max" to "% of Rank 1"
    "% of Max" was actually a little misleading, since it could be interpreted here as "% of max possible value" instead of "% of same value for the max stat product spread"

--- data/outputData.ts
+++ data/outputData.ts
@@ -16,7 +16,19 @@ export const OUTPUT_DATA: OutputDataPoint[] = [
   { key: 'rank', name: 'Rank', description: '' },
   { key: 'level', name: 'Level', description: '' },
   { key: 'cp', name: 'CP', description: '' },
-  { key: 'stats', name: 'Stats', description: '' },
-  { key: 'statProduct', name: 'Product', description: '' },
-  { key: 'percent', name: '% of Max', description: '' },
+  {
+    key: 'stats',
+    name: 'Stats',
+    description: '',
+  },
+  {
+    key: 'statProduct',
+    name: 'Stat Product',
+    description: '',
+  },
+  {
+    key: 'percent',
+    name: '% of Rank 1',
+    description: '',
+  },
 ];
