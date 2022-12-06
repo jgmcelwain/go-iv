34c593be97caac78ba153e270fc76450a81e29ff
Jamie McElwain <jgmcelwain@icloud.com>
2022-12-06 14:32:18 +0000

    tweak: update release banner

--- hooks/useReleaseBannerDisplayed/Provider.tsx
+++ hooks/useReleaseBannerDisplayed/Provider.tsx
@@ -2,7 +2,7 @@ import React, { FC, useCallback, useState } from 'react';
 
 import { Context, setReleaseBannerVersionHiddenCookie } from '.';
 
-const CURRENT_RELEASE_BANNER_VERSION = 2;
+const CURRENT_RELEASE_BANNER_VERSION = 3;
 
 function useReleaseBannerState(releaseBannerVersionHidden: number | null) {
   const [isDisplayed, setIsDisplayed] = useState(
