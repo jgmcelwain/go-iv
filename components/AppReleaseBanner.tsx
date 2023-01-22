62b28e4b39d277e22d9d31be7604916e1a85c5d8
Jamie McElwain <jgmcelwain@icloud.com>
2023-01-22 17:32:30 +0000

    fix: release banner should always be dispalyed on top

--- components/AppReleaseBanner.tsx
+++ components/AppReleaseBanner.tsx
@@ -34,7 +34,7 @@ const AppReleaseBanner: FC<{
   if (dismissed || shown === false) return null;
 
   return (
-    <div className='relative bg-emerald-600'>
+    <div className='z-20 relative bg-emerald-600'>
       <div className='mx-auto max-w-8xl py-3 px-4 md:px-8'>
         <div className='pr-16 sm:px-16 sm:text-center'>
           <p className='font-medium text-white text-xs'>
