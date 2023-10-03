63bc6b0986f3a861c2c60182a6bd94b6b0da7ba7
Jamie McElwain <jgmcelwain@icloud.com>
2023-10-03 15:41:11 +0100

    feat: update style and data output

--- features/iv-guess/index.tsx
+++ features/iv-guess/index.tsx
@@ -1,89 +1,146 @@
-import { useEffect, useMemo, useState } from 'react';
-import { SpeciesDropdown } from './species-dropdown';
-import { Pokemon } from '../../data/pokedex';
-import { usePokedex } from '../../hooks/usePokedex';
+import {
+  CartesianGrid,
+  Label,
+  ResponsiveContainer,
+  Scatter,
+  ScatterChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
+
+import { useState } from 'react';
 import { LEAGUES, LeagueCPCap } from '../../data/league';
 import { LEVEL_CAPS, LevelCapNumber } from '../../data/levelCap';
-import { generateRankedSpreads } from '../../lib/generateRankedSpreads';
+import { Pokemon } from '../../data/pokedex';
+import { usePokedex } from '../../hooks/usePokedex';
+import {
+  RankedSpread,
+  generateRankedSpreads,
+} from '../../lib/generateRankedSpreads';
+import { SpeciesDropdown } from './species-dropdown';
+
+const defaultPokemon = {
+  name: 'Medicham',
+  hp: 142,
+  cp: 1500,
+  leagueCp: 1500,
+  levelCap: 51,
+} as const;
 
 export function IVGuess() {
-  const { list, byName } = usePokedex();
-  const [pokemon, setPokemon] = useState<Pokemon>(byName('Medicham')!);
-  const [league, setLeague] = useState<LeagueCPCap>(1500);
-  const [levelCap, setLevelCap] = useState<LevelCapNumber>(51);
-
-  const [hp, setHp] = useState<number>(142);
-  const [cp, setCp] = useState<number>(1500);
-
-  const matchingSpreads = useMemo(
-    () =>
-      generateRankedSpreads(
-        pokemon,
-        pokemon.floor ?? 0,
-        league,
-        levelCap,
-        0,
-        'product',
-      ).filter((spread) => spread.cp === cp && spread.stats.sta.value === hp),
-    [cp, hp, league, levelCap, pokemon],
+  const { byName } = usePokedex();
+  const [pokemon, setPokemon] = useState<Pokemon>(byName(defaultPokemon.name)!);
+  const [league, setLeague] = useState<LeagueCPCap>(defaultPokemon.leagueCp);
+  const [levelCap, setLevelCap] = useState<LevelCapNumber>(
+    defaultPokemon.levelCap,
   );
 
-  const extremeStats = useMemo(() => {
-    const atk = matchingSpreads.map((spread) => spread.stats.atk.value);
-    const def = matchingSpreads.map((spread) => spread.stats.def.value);
+  const [anyHp, setAnyHp] = useState<boolean>(false);
+  const [hp, setHp] = useState<number>(defaultPokemon.hp);
+  const [anyCp, setAnyCp] = useState<boolean>(false);
+  const [cp, setCp] = useState<number>(defaultPokemon.cp);
+
+  const matchingSpreads = generateRankedSpreads(
+    pokemon,
+    pokemon.floor ?? 0,
+    league,
+    levelCap,
+    0,
+    'product',
+  ).filter(
+    (spread) =>
+      (spread.stats.sta.value === hp || anyHp) && (spread.cp === cp || anyCp),
+  );
+
+  const extremeStats = (() => {
+    const atk = [...matchingSpreads].sort(
+      (a, b) => b.stats.atk.value - a.stats.atk.value,
+    );
+    const def = [...matchingSpreads].sort(
+      (a, b) => b.stats.def.value - a.stats.def.value,
+    );
 
     return {
       atk: {
-        max: Math.max(...atk),
-        min: Math.min(...atk),
+        max: atk.at(0),
+        min: atk.at(-1),
       },
       def: {
-        max: Math.max(...def),
-        min: Math.min(...def),
+        max: def.at(0),
+        min: def.at(-1),
       },
     };
-  }, [matchingSpreads]);
+  })();
 
   return (
-    <div className='w-full grid grid-cols-3 gap-6'>
+    <div className='w-full px-4 sm:px-0 grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-5 gap-6 items-start'>
       <SpeciesDropdown value={pokemon} onChange={setPokemon} label='Pokémon' />
 
-      <label className='block'>
-        <span className='text-xs text-gray-500 dark:text-gray-400'>CP</span>
+      <div className='space-y-2'>
+        <label className='block'>
+          <span className='text-xs text-gray-500 dark:text-gray-400'>CP</span>
 
-        <input
-          onChange={(evt) => {
-            const value = Number(evt.target.value);
+          <input
+            onChange={(evt) => {
+              const value = Number(evt.target.value);
 
-            if (!Number.isNaN(value)) {
-              setCp(value);
-            }
-          }}
-          defaultValue={cp}
-          max={league}
-          className='block w-full mt-1 rounded form-input focus-ring ring-offset-gray-50 dark:ring-offset-gray-900'
-          type='number'
-        />
-      </label>
+              if (!Number.isNaN(value)) {
+                setCp(value);
+              }
+            }}
+            defaultValue={cp}
+            max={league}
+            className='block w-full mt-1 rounded form-input focus-ring ring-offset-gray-50 dark:ring-offset-gray-900 disabled:opacity-50 disabled:cursor-not-allowed'
+            type='number'
+            disabled={anyCp}
+          />
+        </label>
 
-      <label className='block'>
-        <span className='text-xs text-gray-500 dark:text-gray-400'>
-          HP Stat
-        </span>
+        <label className='flex justify-start items-center gap-x-2'>
+          <span className='text-xs text-gray-500 dark:text-gray-400'>
+            Any CP
+          </span>
 
-        <input
-          onChange={(evt) => {
-            const value = Number(evt.target.value);
+          <input
+            onChange={(evt) => setAnyCp(evt.target.checked)}
+            defaultChecked={anyCp}
+            type='checkbox'
+          />
+        </label>
+      </div>
 
-            if (!Number.isNaN(value)) {
-              setHp(value);
-            }
-          }}
-          defaultValue={hp}
-          className='block w-full mt-1 rounded form-input focus-ring ring-offset-gray-50 dark:ring-offset-gray-900'
-          type='number'
-        />
-      </label>
+      <div className='space-y-2'>
+        <label className='block'>
+          <span className='text-xs text-gray-500 dark:text-gray-400'>
+            HP Stat
+          </span>
+
+          <input
+            onChange={(evt) => {
+              const value = Number(evt.target.value);
+
+              if (!Number.isNaN(value)) {
+                setHp(value);
+              }
+            }}
+            defaultValue={hp}
+            className='block w-full mt-1 rounded form-input focus-ring ring-offset-gray-50 dark:ring-offset-gray-900'
+            type='number'
+          />
+        </label>
+        <label className='flex justify-start items-center gap-x-2'>
+          <span className='text-xs text-gray-500 dark:text-gray-400'>
+            Any HP
+          </span>
+
+          <input
+            onChange={(evt) => setAnyHp(evt.target.checked)}
+            defaultChecked={anyHp}
+            type='checkbox'
+          />
+        </label>
+      </div>
 
       <label className='block'>
         <span className='text-xs text-gray-500 dark:text-gray-400'>League</span>
@@ -121,30 +178,171 @@ export function IVGuess() {
         </select>
       </label>
 
-      <div className='col-start-1'>
-        <p>Possible Spreads: {matchingSpreads.length}</p>
+      <div className='col-span-1 md:col-span-3 2xl:col-span-2 w-full shadow-md shadow-gray-100 dark:shadow-xl bg-white dark:bg-gray-800 p-4 dark:shadow-gray-900 rounded-lg border-y border-gray-200 dark:border-transparent border'>
+        <p className='font-title border-b border-gray-100 dark:border-gray-700 pb-2 mb-2'>
+          Possible Spreads: {matchingSpreads.length}{' '}
+          <span className='text-xs'>of 4096</span>
+        </p>
 
-        <ul className='list-disc list-outside'>
-          {matchingSpreads.map((spread) => (
-            <li key={`${spread.rank}-${spread.cp}`}>
-              #{spread.rank} - {spread.ivs.atk} / {spread.ivs.def} /{' '}
-              {spread.ivs.sta} - {spread.stats.atk.value.toFixed(2)}
-            </li>
-          ))}
-        </ul>
+        <table className='w-full table-fixed text-left tabular-nums text-sm'>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>IVs</th>
+              <th>Atk</th>
+              <th>Def</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {matchingSpreads.map((spread) => (
+              <tr key={`${spread.ivs.atk}-${spread.ivs.def}-${spread.ivs.sta}`}>
+                <td>{spread.rank}</td>
+                <td>
+                  {spread.ivs.atk}-{spread.ivs.def}-{spread.ivs.sta}
+                </td>
+                <td>{spread.stats.atk.value.toFixed(2)}</td>
+                <td>{spread.stats.def.value.toFixed(2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
 
       {matchingSpreads.length > 0 && (
         <>
-          <div>
-            <p>Max Atk - {extremeStats.atk.max.toFixed(2)}</p>
+          <div className='col-span-1 md:col-span-3 w-full overflow-hidden shadow-md shadow-gray-100 dark:shadow-xl bg-white dark:bg-gray-800 p-4 dark:shadow-gray-900 rounded-lg border-y border-gray-200 dark:border-transparent border'>
+            <div className='pt-2 pb-6 text-center w-full flex justify-center items-start flex-wrap gap-x-4 gap-y-8'>
+              {extremeStats.atk.max && extremeStats.atk.min && (
+                <div className='px-12 flex justify-center gap-6 items-end'>
+                  <div>
+                    <p className='text-xs mb-0.5'>Max</p>
 
-            <p>Min Atk - {extremeStats.atk.min.toFixed(2)}</p>
-          </div>
-          <div>
-            <p>Max Def - {extremeStats.def.max.toFixed(2)}</p>
+                    <p className='font-title text-2xl leading-none tabular-nums'>
+                      {extremeStats.atk.max.stats.atk.value.toFixed(2)}
+                    </p>
+                  </div>
 
-            <p>Min Def - {extremeStats.def.min.toFixed(2)}</p>
+                  <p className='font-title leading-6'>Attack</p>
+
+                  <div>
+                    <p className='text-xs mb-0.5'>Min</p>
+
+                    <p className='font-title text-2xl leading-none tabular-nums'>
+                      {extremeStats.atk.min.stats.atk.value.toFixed(2)}
+                    </p>
+                  </div>
+                </div>
+              )}
+
+              {extremeStats.def.max && extremeStats.def.min && (
+                <div className='px-12 flex justify-center gap-6 items-end'>
+                  <div>
+                    <p className='text-xs mb-0.5'>Max</p>
+
+                    <p className='font-title text-2xl leading-none tabular-nums'>
+                      {extremeStats.def.max.stats.def.value.toFixed(2)}
+                    </p>
+                  </div>
+
+                  <p className='font-title leading-6'>Defense</p>
+
+                  <div>
+                    <p className='text-xs mb-0.5'>Min</p>
+
+                    <p className='font-title text-2xl leading-none tabular-nums'>
+                      {extremeStats.def.min.stats.def.value.toFixed(2)}
+                    </p>
+                  </div>
+                </div>
+              )}
+            </div>
+
+            <ResponsiveContainer width='100%' height={300}>
+              <ScatterChart margin={{ bottom: 20, left: 20 }}>
+                <CartesianGrid
+                  strokeDasharray='3 3'
+                  stroke='currentColor'
+                  className='text-gray-300 dark:text-gray-600'
+                />
+
+                <XAxis
+                  type='number'
+                  dataKey='def'
+                  name='Defense'
+                  tickCount={8}
+                  interval='preserveStartEnd'
+                  domain={[
+                    (dataMin: number) => Math.floor(dataMin / 2) * 2,
+                    (dataMax: number) => Math.ceil(dataMax / 2) * 2,
+                  ]}
+                  stroke='currentColor'
+                  className='text-gray-500 dark:text-gray-400'
+                >
+                  <Label value='Defense Stat' offset={0} position='bottom' />
+                </XAxis>
+
+                <YAxis
+                  type='number'
+                  dataKey='atk'
+                  name='Attack'
+                  tickCount={5}
+                  interval='preserveStartEnd'
+                  domain={[
+                    (dataMin: number) => Math.floor(dataMin / 2) * 2,
+                    (dataMax: number) => Math.ceil(dataMax / 2) * 2,
+                  ]}
+                  stroke='currentColor'
+                  className='text-gray-500 dark:text-gray-400'
+                >
+                  <Label
+                    value='Attack Stat'
+                    offset={0}
+                    angle={270}
+                    position='insideLeft'
+                    style={{ textAnchor: 'middle' }}
+                  />
+                </YAxis>
+
+                <Tooltip
+                  cursor={{ strokeDasharray: '3 3' }}
+                  content={({ active, payload }) => {
+                    if (
+                      active === false ||
+                      payload === undefined ||
+                      payload.length === 0
+                    ) {
+                      return null;
+                    }
+
+                    const spread = payload[0].payload as RankedSpread;
+
+                    return (
+                      <div className='p-2 text-sm space-y-1 shadow-md shadow-gray-100 dark:shadow-xl bg-white dark:bg-gray-800 dark:shadow-gray-900 rounded-lg border-y border-gray-200 dark:border-transparent border'>
+                        <p className='font-semibold'>
+                          {spread.ivs.atk}-{spread.ivs.def}-{spread.ivs.sta}{' '}
+                          (Rank {spread.rank})
+                        </p>
+
+                        <p>Attack: {spread.stats.atk.value.toFixed(2)}</p>
+                        <p>Defense: {spread.stats.def.value.toFixed(2)}</p>
+                        <p>HP: {spread.stats.sta.value}</p>
+                      </div>
+                    );
+                  }}
+                />
+
+                <Scatter
+                  data={matchingSpreads.map((spread) => ({
+                    ...spread,
+                    atk: spread.stats.atk.value,
+                    def: spread.stats.def.value,
+                  }))}
+                  fill='#8884d8'
+                  animationDuration={100}
+                />
+              </ScatterChart>
+            </ResponsiveContainer>
           </div>
         </>
       )}
--- pages/iv-guess.tsx
+++ pages/iv-guess.tsx
@@ -3,11 +3,14 @@ import { IVGuess } from '../features/iv-guess';
 export default function IVGuessPage() {
   return (
     <>
-      <h1 className='w-full text-2xl pt-2 font-semibold'>IV Guess</h1>
+      <h1 className='px-4 sm:px-0 w-full text-2xl pt-2 font-semibold'>
+        IV Guess{' '}
+        <span className='text-xs dark:text-gray-500 text-gray-400'>Beta</span>
+      </h1>
 
-      <p className='mb-2 text-sm text-gray-700 dark:text-gray-300'>
-        This tool is a work-in-progress. Obviously you can't use it in a
-        tournament.
+      <p className='w-full px-4 sm:px-0 mb-2 text-sm text-gray-700 dark:text-gray-300'>
+        This tool is a work-in-progress and should be used for educational
+        purposes only. Please don't try to use this in an official tournament!
       </p>
 
       <IVGuess />
